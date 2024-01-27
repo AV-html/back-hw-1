9974687b237e2f15a536426694c06fabbd827290
Alexander Volkov <AV-html@yandex.ru>
2024-01-27 17:43:39 +0300

    1 спринт 1 часть (настроил router)

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,6 @@
 import express from 'express'
-import { ICreateVideoReq, IErrors, IVideoDbType, RequestWithBody, RequestWithParams } from "./types";
+import { IVideoDbType } from "./types";
+import { videosRouter } from "./routes/videos-router";
 
 export const app = express()
 
@@ -15,7 +16,7 @@ export const ResolutionsList = [
   "P1440",
   "P2160"
 ]
-const videos: IVideoDbType[] = [
+export const videos: IVideoDbType[] = [
   {
     id: 0,
     title: '',
@@ -30,80 +31,10 @@ const videos: IVideoDbType[] = [
   }
 ]
 
-app.get('/videos', (req, res) => {
-  res.send(videos)
-})
-
-app.get('/videos/:id', (req: RequestWithParams<{ id: string }>, res) => {
-  const id = +req.params.id
-  const video = videos.find((video) => video.id === id)
-
-  if (!video) {
-    res.sendStatus(404)
-    return
-  }
-  res.send(video)
-})
-
-app.post('/videos', (req: RequestWithBody<ICreateVideoReq>, res) => {
-  const errors: IErrors = {
-    errorMessages: []
-  }
-
-  const {title, author, availableResolutions} = req.body
-
-  if (!title || typeof title !== 'string' ||!title.trim() || title.trim().length > 40) {
-    errors.errorMessages.push({
-      field: 'title',
-      message: 'Invalid title'
-    })
-  }
-
-  if (!author || typeof author !== 'string' || !author.trim() || author.trim().length > 20) {
-    errors.errorMessages.push({
-      field: 'author',
-      message: 'Invalid author'
-    })
-  }
-
-  let totalAvailableResolutions = availableResolutions
-  if (availableResolutions && Array.isArray(availableResolutions)) {
-    availableResolutions.forEach((resolution) => {
-      !ResolutionsList.includes(resolution) && errors.errorMessages.push({
-        field: 'availableResolutions',
-        message: `Invalid resolution ${resolution}!`
-      })
-    })
-  } else {
-    totalAvailableResolutions = []
-  }
-
-  if (errors.errorMessages.length) {
-    res.status(400).send(errors)
-    return
-  }
-
-  const createdAt = new Date()
-  const publicationDate = new Date()
-  publicationDate.setDate(createdAt.getDate() + 1)
-
-  const newVideo: IVideoDbType = {
-    id: +publicationDate,
-    availableResolutions: totalAvailableResolutions,
-    canBeDownloaded: false,
-    createdAt: createdAt.toISOString(),
-    minAgeRegistration: null,
-    publicationDate: publicationDate.toISOString(),
-    title,
-    author,
-  }
-
-  videos.push(newVideo)
-
-  res.status(201).send(newVideo)
-})
 
 app.delete('/testing/all-data', (req, res) => {
   videos.length = 0
   res.sendStatus(204)
 })
+
+app.use('/videos', videosRouter)
